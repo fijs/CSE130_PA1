0949ee038d2c3409b767fd11c565e438dd7a5bda
Fernando Isaac Jaime <fijs90@gmail.com>
2016-01-21 01:47:30 -0800

    assoc took waaaay too long

--- pa2/misc.ml
+++ pa2/misc.ml
@@ -1,10 +1,38 @@
 (* CSE 130: Programming Assignment 2
+   Fernando I Jaime, A11643783
  * misc.ml
  *)
 
+(* UBER-HELPER FUNCTION
+let rec fold f elements_so_far elements_remaining k = 
+  match elements_remaining with
+  | [] -> elements_so_far
+  | head::tail -> fold f (f elements_so_far head k) tail k;;
+*)
+
+(* HELPER FOR ASSOC 
+let assocHelper d k (r1,r2) = 
+  if r1 = k then r2
+  else d;;
+*)
+
 (* ***** DOCUMENT ALL FUNCTIONS YOU WRITE OR COMPLETE ***** *)
 
-let rec assoc (d,k,l) = failwith "to be written"
+let rec assoc (d,k,l) = 
+  let rec helper k default_value elements_remaining = 
+    match elements_remaining with
+    (* Will return value d if no k_i matches k *)
+    | [] -> default_value
+    | head::tail -> 
+    ( 
+      let (k_i,v_i) = head in
+      (* Value found and we can exit recursion *)
+      if k_i = k then v_i
+      (* Otherwise continue *)
+      else helper k default_value tail
+    )
+  in 
+  helper k d l;; 
 
 (* fill in the code wherever it says : failwith "to be written" *)
 let removeDuplicates l = 
